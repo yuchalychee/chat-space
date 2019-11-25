c159790fb935c1782a674397fc20bd79a1b4896b
yuchalycheee <yuchalychee@yahoo.co>
2019-11-25 16:24:12 +0900

    非同期投稿の実装__修正2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,9 +19,9 @@ $(function(){
     var formData = new FormData(this);
     var url = $(this).attr('action');
     
-    $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
+  $.ajax({
+    url: url,  //同期通信でいう『パス』
+    type: 'POST',  //同期通信でいう『HTTPメソッド』
     data: formData,  
     dataType: 'json',
     processData: false,
